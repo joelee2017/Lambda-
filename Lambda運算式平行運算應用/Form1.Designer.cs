4659d867b2adb2993a656fd5f4fb2b9dbce934ad
joelee2017 <as955133@gmail.com>
2017-12-13 14:29:33 +0800

    PUSH

--- Lambda運算式平行運算應用/Form1.Designer.cs
+++ Lambda運算式平行運算應用/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnInvoke = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnInvoke
+            // 
+            this.btnInvoke.Location = new System.Drawing.Point(388, 36);
+            this.btnInvoke.Name = "btnInvoke";
+            this.btnInvoke.Size = new System.Drawing.Size(75, 23);
+            this.btnInvoke.TabIndex = 0;
+            this.btnInvoke.Text = "button1";
+            this.btnInvoke.UseVisualStyleBackColor = true;
+            this.btnInvoke.Click += new System.EventHandler(this.btnInvoke_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(28, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(293, 184);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 357);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnInvoke);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnInvoke;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Lambda運算式平行運算應用/Form1.cs
+++ Lambda運算式平行運算應用/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,42 @@ namespace Lambda運算式平行運算應用
         public Form1()
         {
             InitializeComponent();
+        }
+
+        BlockingCollection<string> bc = new BlockingCollection<string>();
+        //平行運算專用泛型集合BlockingCollection
+
+        private void btnInvoke_Click(object sender, EventArgs e)
+        {
+            Parallel.Invoke(() => GetOdd(), () => GetEven());
+            //Parallel平算運行，同時運算(GetOdd & GetEven)
+
+            listBox1.DataSource = bc.ToArray();
+        }
+
+        string GetOdd()
+        {
+            for(int i=0; i<=1000;i++)
+            {
+                if(i % 2==1)
+                {
+                    bc.Add($"{i} 奇數");
+                }
+            }
+            return "GetOdd結束 !";
+        }
+
+
+        string GetEven()
+        {
+            for (int i = 0; i <= 1000; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    bc.Add($"{i} 偶數");
+                }
+            }
+            return "GetEven結束 !";
         }
     }
 }
